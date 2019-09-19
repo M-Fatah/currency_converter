d3bce0e2b08e81bee9d7f1acc2ebdd7555b80390
M-Fatah <MuhammadA.Fatah@gmail.com>
2019-09-19 12:23:36 +0200

    Initial commit

---
+++ Assets/Scripts/CurrencyManager.cs
@@ -0,0 +1,310 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using Newtonsoft.Json.Linq;
+using UnityEngine.UI;
+
+public class CurrencyManager : MonoBehaviour
+{
+    [Header("Currency 1")]
+    public InputField inputField1;
+    public Dropdown currencyDrpDwn1;
+    [Header("Currency 2")]
+    public InputField inputField2;
+    public Dropdown currencyDrpDwn2;
+    [Header("Date picker")]
+    public DatePicker datePicker;
+
+
+    private string[] currencyNames = {"USD", "CAD", "GBP", "HKD",  
+                                      "HUF", "CZK", "AUD", "RON", "SEK",
+                                      "IDR", "INR", "BRL", "RUB", "HRK",
+                                      "JPY", "THB", "CHF", "SGD", "PLN",
+                                      "BGN", "TRY", "CNY", "NOK", "NZD",
+                                      "ZAR", "PHP", "MXN", "ILS", "ISK",
+                                      "KRW", "MYR", "DKK",};
+
+    private Currency currency;
+
+    private int baseCurrencyIndex = 0;
+    private int altCurrencyIndex = 0;
+
+    private float baseCurrencyAmount = 1;
+    private float altCurrencyAmount = 0;
+
+    private Dictionary<string, string> queriedData;
+
+    void Start()
+    {
+        currency = new Currency();
+        currency.rates = new Rate[currencyNames.Length];
+        queriedData = new Dictionary<string, string>();
+        
+        // Populate Dropdowns with array data at startup.
+        if(currencyDrpDwn1 & currencyDrpDwn2)
+        {
+            currencyDrpDwn1.ClearOptions();
+            currencyDrpDwn2.ClearOptions();
+
+            for (int i = 0; i < currencyNames.Length; i++)
+            {
+                currencyDrpDwn1.options.Add(new Dropdown.OptionData(currencyNames[i]));
+                currencyDrpDwn2.options.Add(new Dropdown.OptionData(currencyNames[i]));
+            }
+        }
+
+        // Get previously chosen currencies if there is any otherwise choose default values.
+        baseCurrencyIndex = PlayerPrefs.GetInt("CURR1", 1);
+        altCurrencyIndex = PlayerPrefs.GetInt("CURR2", 2);
+
+        currencyDrpDwn1.value = baseCurrencyIndex;
+        currencyDrpDwn2.value = altCurrencyIndex;
+
+
+        OnDrpDwn1ValueChanged(baseCurrencyIndex);
+        OnDrpDwn2ValueChanged(altCurrencyIndex);
+
+        //GetCurrencyRates(currencyNames[baseCurrencyIndex]);
+    }
+
+    /// <summary>
+    /// Called whenever the user changes the value on currency1 dropdown.
+    /// </summary>
+    /// <param name="v"> The new dropdown index of the value chosen. </param>
+    public void OnDrpDwn1ValueChanged(int v)
+    {
+        PlayerPrefs.SetInt("CURR1", v);
+        PlayerPrefs.Save();
+
+        baseCurrencyIndex = v;
+
+        GetCurrencyRates(currencyNames[baseCurrencyIndex]);
+
+        altCurrencyAmount = Calculate(baseCurrencyAmount, altCurrencyIndex);
+
+        if (inputField1)
+        {
+            inputField1.text = baseCurrencyAmount.ToString();
+        }
+
+        if (inputField2)
+        {
+            inputField2.text = altCurrencyAmount.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Called whenever the user changes the value on currency2 dropdown.
+    /// </summary>
+    /// <param name="v"> The new dropdown index of the value chosen. </param>
+    public void OnDrpDwn2ValueChanged(int v)
+    {
+        PlayerPrefs.SetInt("CURR2", v);
+        PlayerPrefs.Save();
+
+        altCurrencyIndex = v;
+
+        altCurrencyAmount = Calculate(baseCurrencyAmount, altCurrencyIndex);
+
+        if (inputField1)
+        {
+            inputField1.text = baseCurrencyAmount.ToString();
+        }
+
+        if (inputField2)
+        {
+            inputField2.text = altCurrencyAmount.ToString();
+        }
+
+    }
+
+    /// <summary>
+    /// Called whenever the user inputs a value on currency1 InputField.
+    /// </summary>
+    /// <param name="t"> the value of the user input. </param>
+    public void OnInput1ValueChanged(string t)
+    {
+        baseCurrencyAmount = float.Parse(t);
+
+        altCurrencyAmount = Calculate(baseCurrencyAmount, altCurrencyIndex);
+
+        if (inputField1)
+        {
+            inputField1.text = baseCurrencyAmount.ToString();
+        }
+
+        if (inputField2)
+        {
+            inputField2.text = altCurrencyAmount.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Called whenever the user inputs a value on currency2 InputField.
+    /// </summary>
+    /// <param name="t"> the value of the user input. </param>
+    public void OnInput2ValueChanged(string t)
+    {
+        altCurrencyAmount = float.Parse(t);
+
+        baseCurrencyAmount = Calculate(altCurrencyAmount / currency.rates[altCurrencyIndex].value, baseCurrencyIndex);
+
+        if (inputField1)
+        {
+            inputField1.text = baseCurrencyAmount.ToString();
+        }
+
+        if (inputField2)
+        {
+            inputField2.text = altCurrencyAmount.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Swaps currency values.
+    /// Called when pressing the swap button.
+    /// </summary>
+    public void SwapCurrencies()
+    {
+        if(currencyDrpDwn1 && currencyDrpDwn2)
+        {
+            int tmp = currencyDrpDwn1.value;
+            currencyDrpDwn1.value = currencyDrpDwn2.value;
+            currencyDrpDwn2.value = tmp;
+
+            OnDrpDwn1ValueChanged(currencyDrpDwn1.value);
+            OnDrpDwn2ValueChanged(currencyDrpDwn2.value);
+        }
+    }
+
+    /// <summary>
+    /// Calculate the money amount when converting from a currency to another.
+    /// </summary>
+    /// <param name="currencyAmount"> The money amount to convert. </param>
+    /// <param name="rateIndex"> the rate of the currency that we want to convert to. </param>
+    /// <returns></returns>
+    float Calculate(float currencyAmount, int rateIndex)
+    {
+        if (currency.rates[rateIndex] == null)
+            return 0;
+
+        return currencyAmount * currency.rates[rateIndex].value;
+    }
+
+    /// <summary>
+    /// Query currency rates from @https://api.exchangeratesapi.io
+    /// </summary>
+    /// <param name="baseCurrency"> The currency we want to convert from. </param>
+    void GetCurrencyRates(string baseCurrency)
+    {
+        // Check if data is already queried once before.
+        string key = baseCurrency + datePicker.GetDate();
+
+        // Check if the data is contained in our dictionary, then get it otherwise query new data from the internet.
+        if (queriedData.ContainsKey(key))
+        {
+            // Parse json data.
+            JObject o = JObject.Parse(queriedData[key]);
+
+            currency.name = o["base"].Value<string>();
+            currency.date = o["date"].Value<DateTime>();
+
+            for (int i = 0; i < currency.rates.Length; i++)
+            {
+                string name = currencyNames[i];
+
+                Rate rate = new Rate();
+                rate.name = name;
+                rate.value = o["rates"][name].Value<float>();
+                currency.rates[i] = rate;
+
+                Debug.Log(name + " : " + o["rates"][name].Value<float>());
+            }
+
+            Debug.Log("\n-----------------------------------------------\n");
+
+            Debug.Log("Used data queried once before");
+        }
+        else
+        {
+            string URL = @"https://api.exchangeratesapi.io/latest?base=" + baseCurrency;
+
+            if (datePicker)
+            {
+                Debug.Log("Querying Data as of Date: " + datePicker.GetDate());
+
+                URL = @"https://api.exchangeratesapi.io/" + datePicker.GetDate() + "?base=" + baseCurrency;
+            }
+
+            UnityWebRequest ww = new UnityWebRequest(URL);
+
+            ww.method = "GET";
+
+            ww.SetRequestHeader("Content-Type", "plain/text");
+
+            ww.downloadHandler = new DownloadHandlerBuffer();
+
+            ww.SendWebRequest();
+
+            while (!ww.isDone) ;
+
+            if (!string.IsNullOrEmpty(ww.error))
+            {
+                Debug.Log("Error: getting API request failed, " + ww.error);
+
+                return;
+            }
+
+            //currency = JsonUtility.FromJson<Currency>(ww.downloadHandler.text);
+
+
+            // Cache data for later use.
+            if (!queriedData.ContainsKey(key))
+                queriedData.Add(key, ww.downloadHandler.text);
+
+            // Parse json data.
+            JObject o = JObject.Parse(ww.downloadHandler.text);
+
+            currency.name = o["base"].Value<string>();
+            currency.date = o["date"].Value<DateTime>();
+
+            for (int i = 0; i < currency.rates.Length; i++)
+            {
+                string name = currencyNames[i];
+
+                Rate rate = new Rate();
+                rate.name = name;
+                rate.value = o["rates"][name].Value<float>();
+                currency.rates[i] = rate;
+
+                Debug.Log(name + " : " + o["rates"][name].Value<float>());
+            }
+            Debug.Log("\n-----------------------------------------------\n");
+
+#if false
+            // TODO: Save data to be persistant for a whole day then update the next day.
+
+            PlayerPrefs.SetString("DATE", currency.date.ToString());
+            PlayerPrefs.SetString("DATA", ww.downloadHandler.text);
+            PlayerPrefs.Save();
+#endif
+        }
+    }
+
+    [System.Serializable]
+    public class Currency
+    {
+        public Rate[] rates;
+        public string name;
+        public DateTime date;
+    }
+
+    [System.Serializable]
+    public class Rate
+    {
+        public string name;
+        public float value;
+    }
+}
---
+++ Assets/Scripts/DatePicker.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DatePicker : MonoBehaviour
+{
+    [Header("Day")]
+    public InputField dayInputField;
+    [Header("Month")]
+    public InputField monthInputField;
+    [Header("Year")]
+    public InputField yearInputField;
+
+    private string currentSelectedDay;
+    private string currentSelectedMonth;
+    private string currentSelectedYear;
+
+    private string currentSelectedDate;
+
+    private DateTime today;
+
+    // TODO: check if date is valid.
+    // Date format will be yyyy-mm-dd.
+
+    private void Awake()
+    {
+        today = DateTime.Today;
+        currentSelectedDay = today.Day.ToString();
+        currentSelectedMonth = today.Month.ToString();
+        currentSelectedYear = today.Year.ToString();
+
+        currentSelectedDate = currentSelectedYear + "-" + currentSelectedMonth + "-" + currentSelectedDay;
+
+        if (dayInputField)
+            dayInputField.text = currentSelectedDay;
+
+        if (monthInputField)
+            monthInputField.text = currentSelectedMonth;
+
+        if (yearInputField)
+            yearInputField.text = currentSelectedYear;
+    }
+
+    /// <summary>
+    /// Increments/Decrements day value then update day input field with the new value.
+    /// </summary>
+    /// <param name="direction"> Used to determine if we want to increment (+1) or decrement (-1). </param>
+    public void DayButton(int direction)
+    {
+        int d = int.Parse(currentSelectedDay);
+        d += direction;
+
+        if (d < 1)
+            d = 1;
+
+        if (d > 31)
+            d = 31;
+
+        currentSelectedDay = d.ToString("D2");
+
+        if (dayInputField)
+            dayInputField.text = currentSelectedDay;
+    }
+
+    /// <summary>
+    /// Increments/Decrements month value then update month input field with the new value.
+    /// </summary>
+    /// <param name="direction"> Used to determine if we want to increment (+1) or decrement (-1). </param>
+    public void MonthButton(int direction)
+    {
+        int m = int.Parse(currentSelectedMonth);
+        m += direction;
+
+        if (m < 1)
+            m = 1;
+
+        if (m > 12)
+            m = 12;
+
+        currentSelectedMonth = m.ToString("D2");
+
+        if (monthInputField)
+            monthInputField.text = currentSelectedMonth;
+    }
+
+    /// <summary>
+    /// Increments/Decrements year value then update year input field with the new value.
+    /// </summary>
+    /// <param name="direction"> Used to determine if we want to increment (+1) or decrement (-1). </param>
+    public void YearButton(int direction)
+    {
+        int y = int.Parse(currentSelectedYear);
+        y += direction;
+
+        if (y < 1999)
+            y = 1999;
+
+        if (y > today.Year)
+            y = today.Year;
+
+        currentSelectedYear = y.ToString("D4");
+
+        if (yearInputField)
+            yearInputField.text = currentSelectedYear;
+    }
+
+    /// <summary>
+    /// Updates day input field with the new value;
+    /// </summary>
+    /// <param name="v"> The update value </param>
+    public void OnDayInputChanged(string v)
+    {
+        if (string.IsNullOrEmpty(v) || int.Parse(v) == 0)
+            currentSelectedDay = today.Day.ToString("D2");
+        
+        currentSelectedDay = v;
+
+        if (dayInputField)
+            dayInputField.text = currentSelectedDay;
+    }
+
+    /// <summary>
+    /// Updates month input field with the new value;
+    /// </summary>
+    /// <param name="v"> The update value </param>
+    public void OnMonthInputChanged(string v)
+    {
+        if (string.IsNullOrEmpty(v) || int.Parse(v) == 0)
+            currentSelectedMonth = today.Month.ToString("D2");
+
+        currentSelectedMonth = v;
+
+        if (monthInputField)
+            monthInputField.text = currentSelectedMonth;
+    }
+
+    /// <summary>
+    /// Updates year input field with the new value;
+    /// </summary>
+    /// <param name="v"> The update value </param>
+    public void OnYearInputChanged(string v)
+    {
+        if (string.IsNullOrEmpty(v) || int.Parse(v) == 0)
+            currentSelectedYear = today.Year.ToString("D4");
+
+        currentSelectedYear = v;
+
+        if (yearInputField)
+            yearInputField.text = currentSelectedYear;
+    }
+
+    /// <summary>
+    /// Returns the current date choosen in the date picker.
+    /// </summary>
+    /// <returns></returns>
+    public string GetDate()
+    {
+        currentSelectedDate = currentSelectedYear + "-" + currentSelectedMonth + "-" + currentSelectedDay;
+
+        return currentSelectedDate;
+    }
+}
